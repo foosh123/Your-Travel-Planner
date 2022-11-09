4b249afb69465785ed1b888c26cfefe5288ba714
foosh123 <e0563930@u.nus.edu>
2022-11-09 11:57:28 +0700

    added resetPasswordPage and ForgetPasswordPage

---
+++ ui/src/components/ForgetPassword.vue
@@ -0,0 +1,83 @@
+<template>
+    <div id="container">
+        <form id="form">
+          <p><H1>Please Enter Your Email</H1></p>
+          <form class="credentials">
+            <input
+              type="email"
+              id="email"
+              v-model="emailData"
+              placeholder="Email"
+              size="35"
+            /><br/>
+          </form>
+        </form>    
+        <button id="login" type="button">
+          <span><strong>CONFIRM</strong></span>
+        </button>   
+     
+      </div>
+</template>
+    <script>
+    
+    </script>
+    
+    <!-- Add "scoped" attribute to limit CSS to this component only -->
+    <style scoped>
+    
+    #container {
+      text-align: center;
+      position: relative;
+      bottom: 50px;
+    }
+    
+    /* ------- Sign up form box ------- */
+    #form {
+      border: 3px solid rgb(233, 228, 228, 0.5);
+      position: relative;
+      top: 100px;
+      margin: auto;
+      margin-top: 30px;
+      width: 500px;
+      padding: 10px;
+      height: 250px;
+      text-align: center;
+      box-shadow: 2.5px 2.5px rgb(233, 228, 228, 0.6);
+    }
+    .credentials #email,
+    #password {
+      background: rgba(99, 164, 255, 0.13);
+      border: none;
+      border-radius: 5px;
+      text-align: center;
+      padding-bottom: 10px;
+      padding-top: 10px;
+      font-size: 20px;
+    }
+    
+    /* ------- Forgot password ------- */
+    .forgot-password {
+      margin-right: -300px;
+      font-size: 15px;
+      padding-bottom: 30px;
+      padding-top: 1px;
+    }
+    
+    #password-retrieval {
+      color: #63a4ff;
+    }
+    
+    #login {
+      color: #E50072;
+      box-sizing: border-box;
+      border-radius: 8px;
+      margin: 10px;
+      display: inline-block;
+      font-size: 20px;
+      background-color: white;
+      border: 2px solid #E50072;
+      padding: 12px 30px;
+    }
+    
+    </style>
+    
--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import LoginPage from "@/views/LoginPage.vue";
 import SignupPage from "@/views/SignupPage.vue";
 import HomePage from "@/views/HomePage.vue";
+import ForgetPasswordPage from "@/views/ForgetPassword.vue";
+import ResetPasswordPage from "@/views/ResetPassword.vue";
 
 const routes = [
   {
@@ -18,6 +20,16 @@ const routes = [
     path: "/signup",
     name: "Signup",
     component: SignupPage,
+  },
+  {
+    path: "/forgetpassword",
+    name: "ForgetPassword",
+    component: ForgetPasswordPage,
+  },
+  {
+    path: "/resetpassword",
+    name: "ResetPassword",
+    component: ResetPasswordPage,
   },
 ]
 const router = createRouter({
